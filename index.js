7b1cd0b251b52719a7fb645d9147251b910572e6
jishu825 <2805672171@qq.com>
2024-12-01 21:36:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+// index.js
+
+// Import required modules
 import { initializeUI } from "./scripts/ui.js";
 import { setupEffects } from "./scripts/effects.js";
 import { initializeColorPickers } from "./scripts/colorPicker.js";
 import { setupUtils } from "./scripts/utils.js";
 
-(function() {
+// Wait for SillyTavern to be ready
+window.addEventListener('load', () => {
+    // Initialize extension when the app is ready
+    const interval = setInterval(() => {
+        const themeElements = document.querySelector('div[name="themeElements"]');
+        if (themeElements) {
+            clearInterval(interval);
+            initializeExtension();
+        }
+    }, 100);
+});
+
+function initializeExtension() {
     setupUtils();
     initializeColorPickers();
     setupEffects();
     initializeUI();
-})();
+}
