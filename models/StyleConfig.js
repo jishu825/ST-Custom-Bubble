b45ee40ff3e41496efe967149e166f7d3497cbae
jishu825 <2805672171@qq.com>
2024-12-05 10:10:19 +0800

    Update StyleConfig.js

--- models/StyleConfig.js
+++ models/StyleConfig.js
@@ -1,3 +1,27 @@
+export class StyleConfig {
+    constructor(data = {}) {
+        this.id = data.id || crypto.randomUUID();
+        this.name = data.name || '';
+        this.bubble = new BubbleStyle(data.bubble);
+        this.text = new TextStyle(data.text);
+    }
+
+    static createDefault() {
+        return new StyleConfig({
+            name: 'Default Style'
+        });
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            name: this.name,
+            bubble: this.bubble.toJSON(),
+            text: this.text.toJSON()
+        };
+    }
+}
+
 export class BaseStyle {
     constructor(data = {}) {
         // 复制数据并填充默认值
@@ -54,30 +78,6 @@ export class TextStyle extends BaseStyle {
                 glowColor: '#3388ff',
                 radius: 2
             }
-        };
-    }
-}
-
-export class StyleConfig {
-    constructor(data = {}) {
-        this.id = data.id || crypto.randomUUID();
-        this.name = data.name || '';
-        this.bubble = new BubbleStyle(data.bubble);
-        this.text = new TextStyle(data.text);
-    }
-
-    static createDefault() {
-        return new StyleConfig({
-            name: 'Default Style'
-        });
-    }
-
-    toJSON() {
-        return {
-            id: this.id,
-            name: this.name,
-            bubble: this.bubble.toJSON(),
-            text: this.text.toJSON()
         };
     }
 }
